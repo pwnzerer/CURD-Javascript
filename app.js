2776db96b787a34c34bd1f3ea4d01471d8bb6725
pwnzerer <hamzaidrees2008@gmail.com>
2022-03-04 16:31:36 +0500

    added arrows functions and replaced vars with let

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-function generate_table(anyarray) {
-    var body = document.getElementsByTagName("body")[0];
-    var table = document.createElement("table");
+const generate_table = (anyarray) => {
+    let body = document.getElementsByTagName("body")[0];
+    let table = document.createElement("table");
     document.body.append(table);
-    var tbl = document.getElementsByTagName("table")[0]
+    let tbl = document.getElementsByTagName("table")[0]
     tbl.classList.add('table');
-    var tblBody = document.createElement("tbody");
+    let tblBody = document.createElement("tbody");
     for (const element of anyarray) {
-      var row = document.createElement("tr");
-        var cell = document.createElement("td");
+      let row = document.createElement("tr");
+        let cell = document.createElement("td");
         cell.className = "name";
-        var cell2 = document.createElement("td");
-        var cellText = document.createTextNode(element['name']);
+        let cell2 = document.createElement("td");
+        let cellText = document.createTextNode(element['name']);
         const anybtn = document.createElement("input");
         anybtn.value = 'Delete';
         anybtn.type = 'button';
@@ -34,15 +34,15 @@ function generate_table(anyarray) {
     body.appendChild(tbl);
     tbl.setAttribute("border", "2");
   }
-  function add_book(){
-    var book_el = document.getElementById('add-book');
-    var book_name = book_el.value;
-    var tblBody = document.getElementsByTagName("tbody")[0];
-    var row = document.createElement("tr");
-    var cell = document.createElement("td");
-    var cell2 = document.createElement("td");
+  const add_book = () => {
+    let book_el = document.getElementById('add-book');
+    let book_name = book_el.value;
+    let tblBody = document.getElementsByTagName("tbody")[0];
+    let row = document.createElement("tr");
+    let cell = document.createElement("td");
+    let cell2 = document.createElement("td");
     cell.className = "name";
-    var cellText = document.createTextNode(book_name);
+    let cellText = document.createTextNode(book_name);
     const anybtn = document.createElement("input");
     anybtn.value = 'Delete';
     anybtn.type = 'button';
@@ -65,23 +65,23 @@ function generate_table(anyarray) {
 
 
   function deleteRow() {
-    var index = this.parentNode.parentNode.rowIndex;
+    let index = this.parentNode.parentNode.rowIndex;
     console.log(index);
     console.log(this);
     document.getElementsByTagName("table")[0].deleteRow(index);
   }
 
 // const axios = require('axios').default;
-var data = axios.get('https://the-one-api.dev/v2/book')
+function data() { axios.get('https://the-one-api.dev/v2/book')
   .then(function (response) {
-    // var resdata = response.data;
+    // let resdata = response.data;
     for (const element of response.data.docs){console.log(element);}
     generate_table(response.data.docs);
     console.log(response.data.docs);
     return response;
   })
   .catch(function (error) {
-    console.log(error);})
+    console.log(error);})}
 
     let tableRowElement;
     function toggleModal() {
@@ -109,3 +109,5 @@ var data = axios.get('https://the-one-api.dev/v2/book')
       tableRowElement.getElementsByClassName('name')[0].innerText=name;
       closeModal();
   }
+
+  data();
